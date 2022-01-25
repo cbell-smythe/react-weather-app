2280a89e6d27232711cb438ca7e52dc2535277d9
cbell-smythe <50560779+cbell-smythe@users.noreply.github.com>
2022-01-24 19:18:53 -0500

    Updated the Footer with GitHub repo URL

--- src/Footer.js
+++ src/Footer.js
@@ -5,15 +5,15 @@ export default function Footer() {
   return (
     <div className="Footer">
       <footer>
+        This project was coded by Chrystal Bell-Smythe and is{" "}
         <a
           href="https://github.com/cbell-smythe/react-weather-app"
           target="_blank"
           rel="noreferrer"
           className="plain"
         >
-          Open-source code
-        </a>{" "}
-        by Chrystal Bell-Smythe
+          open-sourced on Github
+        </a>
       </footer>
     </div>
   );
