7c0a3b97cdeb91eaff9bc292e80b0fcb34c4db64
cbell-smythe <50560779+cbell-smythe@users.noreply.github.com>
2022-01-25 19:24:50 -0500

    Moved Search code and css to Weather component and css.

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Weather.css";
-import Search from "./Search";
 
 import CurrentWeather from "./CurrentWeather";
 import HourlyForecast from "./HourlyForecast";
@@ -9,11 +8,40 @@ import AdditionalWeather from "./AdditionalWeather";
 import Footer from "./Footer";
 
 export default function Weather() {
+  let weatherData = {
+    city: "Toronto",
+    date: "monday 7:45 PM",
+  };
   return (
     <div className="Weather">
       <div className="WeatherAppWrapper">
         <div className="WeatherApp">
-          <Search />
+          <div className="Search">
+            <form className="mb-2">
+              <div className="row">
+                <div className="col-9">
+                  <input
+                    type="search"
+                    placeholder="enter a city..."
+                    className="form-control"
+                    autoComplete="off"
+                    autoFocus="on"
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    type="submit"
+                    value="search"
+                    className="btn btn-outline-dark w-100"
+                  />
+                </div>
+              </div>
+            </form>
+            <div className="City">
+              <h1>{weatherData.city}</h1>
+              <h2>last refresh @ {weatherData.date}</h2>
+            </div>
+          </div>
           <div className="row">
             <div className="col-7">
               <CurrentWeather />
