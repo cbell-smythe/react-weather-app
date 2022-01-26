95477595e12bab5baa7362e80b202b334b1055c8
cbell-smythe <50560779+cbell-smythe@users.noreply.github.com>
2022-01-25 19:19:28 -0500

    Removed axios code, as it wasn't in use

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,11 +1,10 @@
 import React from "react";
-import axios from "axios";
 import "./CurrentWeather.css";
 
 export default function CurrentWeather() {
-  let apiKey = "40687d56eaf0d831bbcf4565c75ed97f";
-  let city = "Toronto";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  //let apiKey = "40687d56eaf0d831bbcf4565c75ed97f";
+  //let city = "Toronto";
+  //let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   return (
     <div>
